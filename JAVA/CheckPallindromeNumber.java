d937efac4f4e0cea6af914fc8de83402da2e940a
shashank-h-s <shashankhsappu@gmail.com>
2023-03-02 23:16:03 +0530

    Pallindrome integer

---
+++ JAVA/CheckPallindromeNumber.java
@@ -0,0 +1,21 @@
+public class CheckPallindromeNumber {
+
+	static boolean pallindromeNumber(int num) {
+		// checking pallindrome without converting into string
+		if (num < 0)
+			return false;
+		int temp = num;
+		int rev = 0;
+		while (temp > 0) {
+			rev = rev * 10 + temp % 10;
+			temp /= 10;
+		}
+		return num == rev;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(pallindromeNumber(1001));
+	}
+}
